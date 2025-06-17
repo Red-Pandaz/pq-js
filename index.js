09ad4f835cd73609ca09d59dd1faa6ef7f64fd9e
Red Panda <Deep_Sea_Penguin@protonmail.com>
2025-06-17 15:10:59 -0400

    add Falcon to index

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const { init: initDilithium, cleanup: cleanupDilithium } = require('./dilithium/src/index.js');
 const { init: initSphincs, cleanup: cleanupSphincs } = require('./sphincs/src/index.js');
+const { init: initFalcon, cleanup: cleanupFalcon } = require('./falcon/src/index.js');
 
 async function createPQ() {
   const dilithium = await initDilithium();
   const sphincs = await initSphincs();
-  return { dilithium, sphincs };
+  const falcon = await initFalcon();
+  return { dilithium, sphincs, falcon };
 }
 
 function cleanupPQ() {
   cleanupDilithium();
   cleanupSphincs();
+  cleanupFalcon();
 }
 
 module.exports = { createPQ, cleanupPQ };
